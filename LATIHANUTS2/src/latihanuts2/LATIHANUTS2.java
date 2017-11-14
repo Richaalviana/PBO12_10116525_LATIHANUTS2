6cdd45a874d8c976f04d960934170c2a6fea8637
Richaalviana <32487705+Richaalviana@users.noreply.github.com>
2017-11-14 22:46:09 +0700

    Add files via upload

---
+++ LATIHANUTS2/src/latihanuts2/LATIHANUTS2.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanuts2;
+
+/**
+ *
+ * @author detun-nuist
+ */
+public class LATIHANUTS2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    private String nama;
+    private String alamat;
+    private int transport;
+    private int tunjangan;
+    private int gapok;
+    private int total;
+    
+    public String getNama(){
+        return nama;
+    }
+    
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    
+     public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public int getTransport() {
+        return transport;
+    }
+
+    public void setTransport(int transport) {
+        this.transport = transport;
+    }
+
+    public int getTunjangan() {
+        return tunjangan;
+    }
+
+    public void setTunjangan(int tunjangan) {
+        this.tunjangan = tunjangan;
+    }
+
+    public int getGapok() {
+        return gapok;
+    }
+
+    public void setGapok(int gapok) {
+        this.gapok = gapok;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+    
+       
+}
